b0d5acae32ddf5b56588ae5119149540245a61b5
José Juan Sánchez <josejuan.sanchez@gmail.com>
2023-02-01 22:45:26 +0100

    Se añaden variables de entorno

--- src/db/database.php
+++ src/db/database.php
@@ -1,13 +1,21 @@
 <?php
 
-class Database{
-    const DB_HOST = "mysql";
-    const DB_NAME = "lamp_db";
-    const DB_USER = "lamp_user";
-    const DB_PASSWORD = "lamp_password";
+class Database {
+	private static $DB_HOST;
+	private static $DB_NAME;
+	private static $DB_USER;
+	private static $DB_PASSWORD;
 
-	public static function connect(){
-		$db = new mysqli(self::DB_HOST, self::DB_USER, self::DB_PASSWORD, self::DB_NAME);
+	public static function init() {
+		self::$DB_HOST = getenv('DB_HOST');
+		self::$DB_NAME = getenv('DB_NAME');
+		self::$DB_USER = getenv('DB_USER');
+		self::$DB_PASSWORD = getenv('DB_PASSWORD');
+	}
+
+	public static function connect() {
+		self::init();
+		$db = new mysqli(self::$DB_HOST, self::$DB_USER, self::$DB_PASSWORD, self::$DB_NAME);
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
